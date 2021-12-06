66be380c77b811067cae42268e77a919566b3849
Noe <bboynoe05@gmail.com>
2021-12-06 00:36:42 -0800

    Updates from Sunday

--- cypress/integration/examples/Test8Framework.js
+++ cypress/integration/examples/Test8Framework.js
@@ -1,6 +1,4 @@
 ///<reference types="cypress"/>
-import data from '../../fixtures/example.json';
-
 
 describe('Hooks and fixtures testing suite', () => {
 
@@ -13,11 +11,31 @@ describe('Hooks and fixtures testing suite', () => {
         })
     });
     
-    it('Fixture test', () => {
-              
+    it('Fixture test and adding custom functions', () => {    
         cy.visit('https://rahulshettyacademy.com/angularpractice/');    
         cy.get(':input[name="name"]:nth-child(2)').type(globalThis.data.name);
         cy.get('select').select(globalThis.data.gender);
-        
+        cy.get(':input[name="name"]:nth-child(1)').should('have.value', globalThis.data.name);
+
+        //ASSERTION FOR MINIMUM LENGTH ATTRIBUTE
+        cy.get(':input[name="name"]:nth-child(2)').should('have.attr', 'minlength', '2');
+        //ALTERNATE WAY OF ASSERTING ATTRIBUTE VALUE
+        cy.get(':input[name="name"]:nth-child(2)').then((el) => {
+            const newElement = el.attr('minlength');
+            expect(newElement).to.equal('2');
+        });
+
+        //ASSERTION FOR DISABLED RADIO BUTTON
+        cy.get('#inlineRadio3').should('be.disabled');
+
+        //GO TO STORE SHOP
+        cy.get(':nth-child(2) > .nav-link').click();
+        cy.get('h4.card-title').each(($el, index, list) => {
+            if($el.text().includes('Blackberry')){
+                //INDEX SERVES AS BUTTON THAT IS IN GROUP WITH BLACKBERRY
+                cy.get('button.btn.btn-info').eq(index).click();
+            }
+        });
+
     });
 });
